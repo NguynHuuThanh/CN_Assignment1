7afc5fd20d84e8a07853fbd618a9d9f86df98898
Temtem <34886648+tamnd04@users.noreply.github.com>
2024-11-18 20:02:17 +0700

    Add files via upload

--- tracker.java
+++ tracker.java
@@ -5,10 +5,9 @@ import java.security.NoSuchAlgorithmException;
 import java.util.*;
 
 public class MyServer {
-    private static HashMap<String, ArrayList<HashMap<String, ArrayList<String>>>> clientMap = new HashMap<>();
-    public static final String BASE_PATH = "D:" + File.separator + "Tamnd" + File.separator + 
-                                          "Tailieudh" + File.separator + "Computer network" + 
-                                          File.separator + "Project" + File.separator;
+    // Map<info_hash, Map<pieceIndex, List<clientKey>>>
+    public static HashMap<String, HashMap<Integer, ArrayList<String>>> file_list = new HashMap<>();
+    public static final String BASE_PATH = "D:" + File.separator + "Tamnd" + File.separator + "Tailieudh" + File.separator + "Computer network" + File.separator + "Project" + File.separator;
 
     public static void main(String[] args) {
         try {
@@ -32,18 +31,18 @@ public class MyServer {
         }
     }
 
-    public static void addClient(String key, ArrayList<HashMap<String, ArrayList<String>>> clientInfo) {
-        clientMap.put(key, clientInfo);
-    }
-
     public static String calculateInfoHash(MetaInfo.InfoDictionary info) {
         try {
+            Map<String, Object> sortedInfoDict = new TreeMap<>();
+            Map<String, Object> originalDict = info.toBEncodedDictionary();
+            sortedInfoDict.putAll(originalDict);
+            
             BEncoder encoder = new BEncoder();
-            encoder.write(info.toBEncodedDictionary());
+            encoder.write(sortedInfoDict);
             String encodedInfo = encoder.toString();
             
             byte[] hash = MetaInfo.calculatePieceHash(encodedInfo.getBytes(StandardCharsets.UTF_8));
-            
+            System.out.println(bytesToHex(hash));
             return URLEncoder.encode(new String(hash, StandardCharsets.ISO_8859_1), StandardCharsets.UTF_8.toString());
         } catch (Exception e) {
             e.printStackTrace();
@@ -51,84 +50,121 @@ public class MyServer {
         }
     }
 
-    public static void createTorrentFile(File sourceFile, String clientPath, int clientPort) throws IOException, NoSuchAlgorithmException {
+    public static String bytesToHex(byte[] bytes) {
+        StringBuilder hexString = new StringBuilder();
+        for (byte b : bytes) {
+            String hex = Integer.toHexString(0xff & b); // Mask the sign bit
+            if (hex.length() == 1) {
+                hexString.append('0'); // Ensure two-character formatting
+            }
+            hexString.append(hex);
+        }
+        return hexString.toString();
+    }
+
+    public static void createTorrentFile(File source, String clientPath, int clientPort) throws IOException, NoSuchAlgorithmException {
         MetaInfo metaInfo = new MetaInfo();
-        
         metaInfo.setAnnounce("http://localhost:" + clientPort);
-        metaInfo.setInfo(sourceFile.getName(), 512000); // 512KB piece length
-        
-        metaInfo.setSingleFile(sourceFile.length());
-        MetaInfo.setFilePieces(metaInfo, sourceFile);
-        
-        String torrentPath = clientPath + File.separator + sourceFile.getName() + ".torrent";
+        metaInfo.setInfo(source.getName(), 512000); // 512KB piece length
+
+        if (source.isDirectory()) {
+            List<MetaInfo.InfoDictionary.FileInfo> files = new ArrayList<>();
+            processDirectory(source, source, files);
+            metaInfo.setMultipleFiles(files);
+        } else {
+            metaInfo.setSingleFile(source.length());
+        }
+
+        MetaInfo.setFilePieces(metaInfo, source);
+        String torrentPath = clientPath + File.separator + source.getName() + ".torrent";
         metaInfo.saveToFile(torrentPath);
     }
 
-    public static void processClientFolder(String clientPath, String clientKey, int clientPort) {
+    private static void processDirectory(File baseDir, File currentDir, List<MetaInfo.InfoDictionary.FileInfo> files) {
+        File[] contents = currentDir.listFiles();
+        if (contents != null) {
+            for (File file : contents) {
+                if (file.isFile()) {
+                    List<String> path = new ArrayList<>();
+                    String relativePath = baseDir.toPath().relativize(file.toPath()).toString();
+                    String[] pathParts = relativePath.split(File.separator);
+                    Collections.addAll(path, pathParts);
+                    files.add(new MetaInfo.InfoDictionary.FileInfo(file.length(), path));
+                } else if (file.isDirectory()) {
+                    processDirectory(baseDir, file, files);
+                }
+            }
+        }
+    }
+
+    public static void processClientFiles(String clientPath, String clientKey) {
         File folder = new File(clientPath);
         if (!folder.exists()) {
             folder.mkdirs();
         }
-
-        ArrayList<HashMap<String, ArrayList<String>>> filesList = new ArrayList<>();
-        File[] files = folder.listFiles();
         
-        if (files != null) {
+        processFilesRecursively(folder, clientKey);
+    }
+
+    private static void processFilesRecursively(File source, String clientKey) {
+        File[] files = source.listFiles();
+        if (files != null){    
             for (File file : files) {
                 if (file.isFile() && !file.getName().endsWith(".torrent")) {
                     try {
-                        File torrentFile = new File(clientPath + File.separator + file.getName() + ".torrent");
+                        File torrentFile = new File(file.getParent(), file.getName() + ".torrent");
                         if (!torrentFile.exists()) {
-                            createTorrentFile(file, clientPath, clientPort);
+                            createTorrentFile(file, file.getParent(), Integer.parseInt(clientKey.split(":")[1]));
                         }
-                        
+        
                         MetaInfo metaInfo = MetaInfo.loadFromFile(torrentFile.getPath());
                         String infoHash = calculateInfoHash(metaInfo.getInfo());
                         
-                        // Create file info
-                        HashMap<String, ArrayList<String>> fileInfo = new HashMap<>();
-                        ArrayList<String> metaInfoData = new ArrayList<>();
-                        metaInfoData.add(file.getName());
-                        metaInfoData.add(String.valueOf(file.length()));
-                        metaInfoData.add(metaInfo.encode());
+                        if (!file_list.containsKey(infoHash)) {
+                            file_list.put(infoHash, new HashMap<>());
+                        }
+        
+                        int totalPieces = metaInfo.getInfo().getPieces().length / 20;
+
+                        System.out.println("File name: " + file.getName());
+                        System.out.println("Number of pieces: " + totalPieces);
                         
-                        fileInfo.put(infoHash, metaInfoData);
-                        filesList.add(fileInfo);
-                        
+                        // Add client as having all pieces
+                        HashMap<Integer, ArrayList<String>> pieceMap = file_list.get(infoHash);
+                        for (int i = 0; i < totalPieces; i++) {
+                            pieceMap.computeIfAbsent(i, k -> new ArrayList<>()).add(clientKey);
+                        }
                     } catch (Exception e) {
                         System.out.println("Error processing file: " + file.getName());
                         e.printStackTrace();
                     }
+                } else if (file.isDirectory()) {
+                    File[] contents = file.listFiles();
+                    if (contents != null) {
+                        processFilesRecursively(file, clientKey);
+                    }
                 }
             }
         }
         
-        addClient(clientKey, filesList);
     }
 
-    public static void printClientMap() {
-        for (Map.Entry<String, ArrayList<HashMap<String, ArrayList<String>>>> entry : clientMap.entrySet()) {
-            String key = entry.getKey();
-            ArrayList<HashMap<String, ArrayList<String>>> list_file = entry.getValue();
+    public static void printFileList() {
+        for (Map.Entry<String, HashMap<Integer, ArrayList<String>>> entry : file_list.entrySet()) {
+            String infoHash = entry.getKey();
+            HashMap<Integer, ArrayList<String>> pieceMap = entry.getValue();
+            System.out.println("\nFile Info Hash: " + infoHash);
+            System.out.println("Piece Distribution:");
             
-            System.out.println("\nClient: " + key);
-            if (list_file.isEmpty()){
-                String[] parts = key.split(":");
-                System.out.println("Client " + parts[1] + " has no file to show!");
-                continue;
+            for (Map.Entry<Integer, ArrayList<String>> pieceEntry : pieceMap.entrySet()) {
+                System.out.println("  Piece " + pieceEntry.getKey() + " is available from clients: " + 
+                                 String.join(", ", pieceEntry.getValue()));
             }
-            else{
-                for (HashMap<String, ArrayList<String>> fileInfo : list_file) {
-                    for (Map.Entry<String, ArrayList<String>> info : fileInfo.entrySet()) {
-                        System.out.println("Info Hash: " + info.getKey());
-                        ArrayList<String> fileData = info.getValue();
-                        System.out.println("  File Name: " + fileData.get(0));
-                        System.out.println("  File Size: " + fileData.get(1) + " bytes");
-                    }
-                }
-            }
-            System.out.println();
         }
+    }
+
+    public static Map<Integer, List<String>> getPieceAvailability(String infoHash) {
+        return new HashMap<>(file_list.getOrDefault(infoHash, new HashMap<>()));
     }
 }
 
@@ -146,16 +182,12 @@ class ClientHandler implements Runnable {
             String clientIP = socket.getInetAddress().getHostAddress();
             clientPort = socket.getPort();
             String key = clientIP + ":" + clientPort;
-
-            // Create client-specific folder path using port number
             String clientPath = MyServer.BASE_PATH + clientPort;
             
-            // Process client folder and create torrent files
-            MyServer.processClientFolder(clientPath, key, clientPort);
-
+            MyServer.processClientFiles(clientPath, key);
             while (true) {
                 String clientMessage = dis.readUTF();
-                MyServer.printClientMap();
+                MyServer.printFileList();
                 if (clientMessage.equalsIgnoreCase("ALIVE")) {
                     System.out.println("Client with IP address " + socket.getInetAddress().getHostAddress() + " and Port: " + clientPort + " is still connected!\n");
                 } else {
@@ -164,20 +196,33 @@ class ClientHandler implements Runnable {
             }
         } catch (EOFException e) {
             System.out.println("Client " + clientPort + " disconnected!");
+            cleanup();
         } catch (IOException e) {
             System.out.println("Client " + clientPort + " disconnected!");
-            shutdown();
+            cleanup();
         }
     }
 
-    public void shutdown() {
-        try{
-         if(!socket.isClosed()){
-            socket.close();
-         }
-
-        }catch (IOException e){
-            e.printStackTrace();
+    private void cleanup() {
+        try {
+            // Remove client's pieces from tracker
+            Iterator<Map.Entry<String, HashMap<Integer, ArrayList<String>>>> iterator = MyServer.file_list.entrySet().iterator();
+            while (iterator.hasNext()) {
+                Map.Entry<String, HashMap<Integer, ArrayList<String>>> entry = iterator.next();
+                HashMap<Integer, ArrayList<String>> pieceMap = entry.getValue();
+                for (ArrayList<String> clients : pieceMap.values()) {
+                    clients.remove(socket.getInetAddress().getHostAddress() + ":" + clientPort);
+                }
+            }
+            
+            // Close resources
+            if (!socket.isClosed()) {
+                socket.close();
+            }
+            
+            System.out.println("Client " + clientPort + " handler cleaned up");
+        } catch (IOException e) {
+            System.out.println("Error during cleanup for client " + clientPort + ": " + e.getMessage());
         }
-     }
+    }
 }
